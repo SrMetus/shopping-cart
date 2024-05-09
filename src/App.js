370cd03c34ce5e2f589169271ba7b722b8227970
Sebastián Gaggero Díaz <s.gaggero87@gmail.com>
2024-05-09 14:03:33 -0400

    Adding a filter for products with all categories and a minimum price

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import products from './mocks/products.json';
 import { Products } from "./components/Products.js";
+import { useState } from 'react';
 
 
 function App() {
+  //const [product] = useState(products);
+  const [filters, setFilters] = useState({
+    category: 'all',
+    minprice: 0
+  });
+
+  const filterProducts = (products) => {
+    return products.filter(product => {
+      return product.price >= product.minprice &&
+      (
+        filters.category === 'all' ||
+        product.category === filters.category
+      )
+    })
+  }
+
   return (
     <Products products={products} />
   );
